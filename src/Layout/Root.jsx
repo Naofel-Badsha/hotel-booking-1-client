d31b925993cb5227c0223105fbf71c7e4553f669
programming hero <naofelbadsha@gmail.com>
2024-11-05 23:28:59 +0000

    added navber new

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,15 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
 import Footer from "../Shared/Footer/Footer";
-import NavbarTow from "../Shared/Navbar/NavbarTow";
+
 
 
 
 const Root = () => {
     return (
         <div>
-            {/* <Navbar></Navbar> */}
-            <NavbarTow></NavbarTow>
+            <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,7 +1,10 @@
-import { useContext } from "react";
-import { FaArrowDown, FaBars } from "react-icons/fa";
+import { useState } from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
+import { FaBars } from "react-icons/fa";
 import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../../Provider/AuthProvider";
+import { useContext } from "react";
 
 const Navbar = () => {
   const { user, logOut } = useContext(AuthContext);
@@ -15,170 +18,218 @@ const Navbar = () => {
       });
   };
 
+  const [dropDownState, setDropDownState] = useState(false);
+  const dropDownMenuRef = useRef();
+
+  useEffect(() => {
+    const closeDropDown = (e) => {
+      if (!dropDownMenuRef?.current?.contains(e?.target)) {
+        setDropDownState(false);
+      }
+    };
+
+    document.addEventListener("mousedown", closeDropDown);
+
+    return () => {
+      document.removeEventListener("mousedown", closeDropDown);
+    };
+  }, []);
+
   return (
-    // className="py-2 bg-opacity-30  text-black shadow-md backdrop-blur-2xl backdrop-saturate-200  fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600"
-
-    <div>
-      <nav className="bg-white border-2 fixed w-full z-20 top-0 start-0">
-        <div className="container m-auto flex flex-wrap items-center justify-between mx-auto p-4">
-          {/*------Logo------*/}
-          <div >
-            <img
-              src="https://i.ibb.co.com/5j6R51X/7-Star-Logo.png"
-              className="w-[70px] h-[70px] object-cover"
-            />
-          </div>
-
-         {/*-----------Profile-------------------*/}
-          <div className="flex gap-3 md:order-2">
-            {/*----------------*/}
-            <img
-              className="w-[40px] h-[40px] rounded-full"
-              src={
-                user?.photoURL
-                  ? user.photoURL
-                  : "https://i.ibb.co/Y79Vw8B/user.png"
-              }
-            />
-            {user ? (
-              <button
-                onClick={handelSingOut}
-                className="btn md:text-xl lg:text-xl text-white font-bold bg-sky-500 rounded-md border-0"
-              >
-                Log Out
-              </button>
-            ) : (
-              <Link to="login">
-                <button className="btn md:text-xl lg:text-xl text-white bg-[#ff6347] font-bold rounded-md border-0">
-                  Login
-                </button>
-              </Link>
-            )}
-            {/*-----------Bar-----Button---------*/}
-            <button
-              data-collapse-toggle="navbar-dropdown"
-              type="button"
-              className="inline-flex items-center  md:hidden"
-              aria-controls="navbar-dropdown"
-              aria-expanded="false"
-            >
-              <FaBars className="text-3xl md:text-4xl text-[#008080ef]"></FaBars>
-            </button>
-          </div>
-
-          <div
-            className="hidden w-full md:block md:w-auto"
-            id="navbar-dropdown"
-          >
-            <ul className="flex flex-col font-medium  p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:space-x-5 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0   dark:border-gray-700">
-              {/*---------Nav------1--------------*/}
-              <li>
-                <NavLink
-                  className="block text-xl py-2 px-3 text-[#008080ef] rounded  md:hover:bg-transparent hover:text-black md:p-0 md:dark:hover:text-black font-bold   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  to="/"
-                >
-                  Home
-                </NavLink>
-              </li>
-
-              <li>
-                <button
-                  id="dropdownNavbarLink"
-                  data-dropdown-toggle="dropdownNavbar"
-                  className="flex items-center justify-between w-full py-2 px-3 text-[#008080ef] text-xl font-bold rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-black md:p-0 md:w-auto dark:hover:text-white md:dark:hover:text-black  dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
-                >
-                  Dropdown <FaArrowDown className="text-xl" />
-                </button>
-
-                {/* <!------------Dropdown menu------------> */}
-                <div
-                  id="dropdownNavbar"
-                  className="z-10 hidden  divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
-                >
-                  <ul
-                    className="p-4  bg-white text-gray-700 dark:text-gray-400"
-                    aria-labelledby="dropdownLargeButton"
-                  >
-                    {/*---------Nav------2--------------*/}
-                    <li className="py-2">
-                      <NavLink
-                        className="block text-xl py-2 px-3 text-[#008080ef] rounded  md:hover:bg-transparent hover:text-black md:p-0 md:dark:hover:text-black font-bold   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-70"
-                        to="/dashboard"
-                      >
-                        DashBoard
-                      </NavLink>
-                    </li>
-                    {/*---------Nav------3--------------*/}
-                    <li className="py-2">
-                      <NavLink
-                        className="block text-xl py-2 px-3 text-[#008080ef] rounded  md:hover:bg-transparent hover:text-black md:p-0 md:dark:hover:text-black font-bold   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-70"
-                        to="/facilities"
-                      >
-                        Facilities
-                      </NavLink>
-                    </li>
-                    {/*---------Nav------4--------------*/}
-                    <li className="py-2">
-                      <NavLink
-                        className="block text-xl py-2 px-3 text-[#008080ef] rounded  md:hover:bg-transparent hover:text-black md:p-0 md:dark:hover:text-black font-bold   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-70"
-                        to="/booking"
-                      >
-                        Booking
-                      </NavLink>
-                    </li>
-                    {/*---------Nav------5--------------*/}
-                    <li className="py-2">
-                      <NavLink
-                        className="block text-xl py-2 px-3 text-[#008080ef] rounded  md:hover:bg-transparent hover:text-black md:p-0 md:dark:hover:text-black font-bold   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-70"
-                        to="/eventsDetails"
-                      >
-                        Events
-                      </NavLink>
-                    </li>
-                    {/* <li>
-                      <NavLink
-                        className="block text-xl py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 font-bold dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                        to="/booking"
-                      >
-                        Booking
-                      </NavLink>
-                    </li> */}
-                  </ul>
-                </div>
-                {/* <!------------Dropdown menu------------> */}
-              </li>
-              {/*---------Nav------6--------------*/}
-              <li>
-                <NavLink
-                  to="/aboutDetails"
-                  className="block text-xl py-2 px-3 text-[#008080ef] rounded  md:hover:bg-transparent hover:text-black md:p-0 md:dark:hover:text-black font-bold   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-70"
-                >
-                  about
-                </NavLink>
-              </li>
-              {/*---------Nav------7--------------*/}
-              <li>
-                <NavLink
-                  className="block text-xl py-2 px-3 text-[#008080ef] rounded  md:hover:bg-transparent hover:text-black md:p-0 md:dark:hover:text-black font-bold   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-70"
-                  to="/room"
-                >
-                  Rooms
-                </NavLink>
-              </li>
-              {/*---------Nav------8--------------*/}
-              <li>
-                <NavLink
-                  className="block text-xl py-2 px-3 text-[#008080ef] rounded  md:hover:bg-transparent hover:text-black md:p-0 md:dark:hover:text-black font-bold   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-70"
-                  to="/contactUs"
-                >
-                  Contact
-                </NavLink>
-              </li>
-            </ul>
-          </div>
+    <nav className="bg-white border-2 fixed w-full z-50 top-0 start-0 flex items-center justify-between  py-4 text-[#393E46]">
+      <div className="scale-100 cursor-pointer rounded-2xl px-3 py-2 text-xl font-semibold text-white transition-all duration-200 hover:scale-110">
+        {/*------Logo------*/}
+        <div>
+          <img
+            src="https://i.ibb.co.com/5j6R51X/7-Star-Logo.png"
+            className="w-[50px] h-[50px] object-cover"
+          />
         </div>
-      </nav>
-    </div>
+      </div>
+      <ul className="hidden items-center justify-between gap-6 lg:flex">
+        {/*---------Nav------1--------------*/}
+        <li>
+          <NavLink
+            className="block text-xl py-2 px-3 text-[#008080ef] rounded  md:hover:bg-transparent hover:text-black md:p-0 md:dark:hover:text-black font-bold   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+            to="/"
+          >
+            Home
+          </NavLink>
+        </li>
+        {/*---------Nav------2--------------*/}
+        <li>
+          <NavLink
+            className="block text-xl py-2 px-3 text-[#008080ef] rounded  md:hover:bg-transparent hover:text-black md:p-0 md:dark:hover:text-black font-bold   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-70"
+            to="/dashboard"
+          >
+            DashBoard
+          </NavLink>
+        </li>
+        {/*---------Nav------3--------------*/}
+        <li>
+          <NavLink
+            className="block text-xl py-2 px-3 text-[#008080ef] rounded  md:hover:bg-transparent hover:text-black md:p-0 md:dark:hover:text-black font-bold   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-70"
+            to="/facilities"
+          >
+            Facilities
+          </NavLink>
+        </li>
+        {/*---------Nav------4--------------*/}
+        <li>
+          <NavLink
+            className="block text-xl py-2 px-3 text-[#008080ef] rounded  md:hover:bg-transparent hover:text-black md:p-0 md:dark:hover:text-black font-bold   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-70"
+            to="/booking"
+          >
+            Booking
+          </NavLink>
+        </li>
+        {/*---------Nav------5--------------*/}
+        <li>
+          <NavLink
+            className="block text-xl py-2 px-3 text-[#008080ef] rounded  md:hover:bg-transparent hover:text-black md:p-0 md:dark:hover:text-black font-bold   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-70"
+            to="/eventsDetails"
+          >
+            Events
+          </NavLink>
+        </li>
+        {/*---------Nav------6--------------*/}
+        <li>
+          <NavLink
+            to="/aboutDetails"
+            className="block text-xl py-2 px-3 text-[#008080ef] rounded  md:hover:bg-transparent hover:text-black md:p-0 md:dark:hover:text-black font-bold   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-70"
+          >
+            about
+          </NavLink>
+        </li>
+        {/*---------Nav------7--------------*/}
+        <li>
+          <NavLink
+            className="block text-xl py-2 px-3 text-[#008080ef] rounded  md:hover:bg-transparent hover:text-black md:p-0 md:dark:hover:text-black font-bold   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-70"
+            to="/room"
+          >
+            Rooms
+          </NavLink>
+        </li>
+        {/*---------Nav------8--------------*/}
+        <li>
+          <NavLink
+            className="block text-xl py-2 px-3 text-[#008080ef] rounded  md:hover:bg-transparent hover:text-black md:p-0 md:dark:hover:text-black font-bold   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-70"
+            to="/contactUs"
+          >
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+      {/*-----------Profile-------------------*/}
+      {/* lg:order-3 */}
+      <div className="flex gap-3 md:justify-end ">
+        {/*----------------*/}
+        <img
+          className="w-[40px] h-[40px] rounded-full"
+          src={
+            user?.photoURL ? user.photoURL : "https://i.ibb.co/Y79Vw8B/user.png"
+          }
+        />
+        {user ? (
+          <button
+            onClick={handelSingOut}
+            className="btn md:text-xl lg:text-xl text-white font-bold bg-sky-500 rounded-md border-0"
+          >
+            Log Out
+          </button>
+        ) : (
+          <Link to="login">
+            <button className="btn md:text-xl lg:text-xl text-white bg-[#ff6347] font-bold rounded-md border-0">
+              Login
+            </button>
+          </Link>
+        )}
+      </div>
+
+      <div
+        ref={dropDownMenuRef}
+        onClick={() => setDropDownState(!dropDownState)}
+        className="relative flex transition-transform lg:hidden"
+      >
+        <FaBars className="text-3xl text-[#008080ef] cursor-pointer"></FaBars>
+        {dropDownState && (
+          <ul className="z-10 gap-2 bg-[#393E46] absolute right-0  px-5 w-[300px] h-[100vh] top-16 flex flex-col">
+            {/*---------Nav------1--------------*/}
+            <li className="mt-10 text-center">
+              <NavLink
+                className="block text-xl py-2 px-3 text-[#008080ef] rounded  md:hover:bg-transparent hover:text-black md:p-0 md:dark:hover:text-black font-bold   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                to="/"
+              >
+                Home
+              </NavLink>
+            </li>
+            {/*---------Nav------2--------------*/}
+            <li className="mt-5 text-center">
+              <NavLink
+                className="block text-xl py-2 px-3 text-[#008080ef] rounded  md:hover:bg-transparent hover:text-black md:p-0 md:dark:hover:text-black font-bold   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-70"
+                to="/dashboard"
+              >
+                DashBoard
+              </NavLink>
+            </li>
+            {/*---------Nav------3--------------*/}
+            <li className="mt-5 text-center">
+              <NavLink
+                className="block text-xl py-2 px-3 text-[#008080ef] rounded  md:hover:bg-transparent hover:text-black md:p-0 md:dark:hover:text-black font-bold   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-70"
+                to="/facilities"
+              >
+                Facilities
+              </NavLink>
+            </li>
+            {/*---------Nav------4--------------*/}
+            <li className="mt-5 text-center">
+              <NavLink
+                className="block text-xl py-2 px-3 text-[#008080ef] rounded  md:hover:bg-transparent hover:text-black md:p-0 md:dark:hover:text-black font-bold   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-70"
+                to="/booking"
+              >
+                Booking
+              </NavLink>
+            </li>
+            {/*---------Nav------5--------------*/}
+            <li className="mt-5 text-center">
+              <NavLink
+                className="block text-xl py-2 px-3 text-[#008080ef] rounded  md:hover:bg-transparent hover:text-black md:p-0 md:dark:hover:text-black font-bold   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-70"
+                to="/eventsDetails"
+              >
+                Events
+              </NavLink>
+            </li>
+            {/*---------Nav------6--------------*/}
+            <li className="mt-5 text-center">
+              <NavLink
+                to="/aboutDetails"
+                className="block text-xl py-2 px-3 text-[#008080ef] rounded  md:hover:bg-transparent hover:text-black md:p-0 md:dark:hover:text-black font-bold   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-70"
+              >
+                about
+              </NavLink>
+            </li>
+            {/*---------Nav------7--------------*/}
+            <li className="mt-5 text-center">
+              <NavLink
+                className="block text-xl py-2 px-3 text-[#008080ef] rounded  md:hover:bg-transparent hover:text-black md:p-0 md:dark:hover:text-black font-bold   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-70"
+                to="/room"
+              >
+                Rooms
+              </NavLink>
+            </li>
+            {/*---------Nav------8--------------*/}
+            <li className="mt-5 text-center">
+              <NavLink
+                className="block text-xl py-2 px-3 text-[#008080ef] rounded  md:hover:bg-transparent hover:text-black md:p-0 md:dark:hover:text-black font-bold   dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-70"
+                to="/contactUs"
+              >
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        )}
+      </div>
+    </nav>
   );
 };
 
